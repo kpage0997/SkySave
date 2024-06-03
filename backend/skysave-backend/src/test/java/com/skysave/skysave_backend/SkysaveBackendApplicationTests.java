72d301e552026b32ec385c4b3e0057de3d21b8cc
kpage0997 <kcpage2@uncg.edu>
2024-06-02 23:39:03 -0400

    backend development folder

--- backend/skysave-backend/src/test/java/com/skysave/skysave_backend/SkysaveBackendApplicationTests.java
+++ backend/skysave-backend/src/test/java/com/skysave/skysave_backend/SkysaveBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backend;
+package com.skysave.skysave_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class SkysaveBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
